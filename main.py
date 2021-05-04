9d6334c3fa5a6c670382b36fac4f123d14ef704d
Kunal Mandil <kunalmandil47@gmail.com>
2021-05-03 23:30:12 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import pyttsx3 #pip install pyttsx3
-import speech_recognition as sr #pip install speechRecognition
+import pyttsx3  # pip install pyttsx3
+import speech_recognition as sr  # pip install speechRecognition
 import datetime
-import wikipedia #pip install wikipedia
+import wikipedia  # pip install wikipedia
 import webbrowser
 import os
 import smtplib
 
 engine = pyttsx3.init('sapi5')
+engine.setProperty("rate", 220)
 voices = engine.getProperty('voices')
 # print(voices[1].id)
 engine.setProperty('voice', voices[0].id)
@@ -19,19 +20,20 @@ def speak(audio):
 
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
+    if hour >= 0 and hour < 12:
         speak("Good Morning!")
 
-    elif hour>=12 and hour<18:
-        speak("Good Afternoon!")   
+    elif hour >= 12 and hour < 18:
+        speak("Good Afternoon!")
 
     else:
-        speak("Good Evening!")  
+        speak("Good Evening!")
 
-    speak("I am smart assistant. How can I help you")       
+    speak("I am smart assistant. How can I help you")
+
 
 def takeCommand():
-    #It takes microphone input from the user and returns string output
+    # It takes microphone input from the user and returns string output
 
     r = sr.Recognizer()
     with sr.Microphone() as source:
@@ -40,15 +42,16 @@ def takeCommand():
         audio = r.listen(source)
 
     try:
-        print("Recognizing...")    
+        print("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
         print(f"User said: {query}\n")
 
     except Exception as e:
-        # print(e)    
-        print("Say that again please...")  
+        # print(e)
+        print("Say that again please...")
         return "None"
     return query
+
 
 def sendEmail(to, content):
     '''
@@ -57,20 +60,24 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('youremail@gmail.com', 'your-password') #make sure to put your email id and password
-    server.sendmail('youremail@gmail.com', to, content) #put the email id of receiver person
+    # make sure to put your email id and password
+    server.login('youremail@gmail.com', 'your-password')
+    # put the email id of receiver person
+    server.sendmail('youremail@gmail.com', to, content)
     server.close()
+
 
 if __name__ == "__main__":
     wishMe()
     while True:
-    # if 1:
+        # if 1:
         query = takeCommand().lower()
 
         # Logic for executing tasks based on query
         if 'wikipedia' in query:
             speak('Searching Wikipedia...')
             query = query.replace("wikipedia", "")
+            print(query)
             results = wikipedia.summary(query, sentences=2)
             speak("According to Wikipedia")
             print(results)
@@ -83,34 +90,33 @@ if __name__ == "__main__":
             webbrowser.open("google.com")
 
         elif 'open stackoverflow' in query:
-            webbrowser.open("stackoverflow.com")   
-
+            webbrowser.open("stackoverflow.com")
 
         elif 'play music' in query:
             music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
             songs = os.listdir(music_dir)
-            print(songs)    
+            print(songs)
             os.startfile(os.path.join(music_dir, songs[0]))
 
         elif 'the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "C:\\Users\\Atit\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code"
             os.startfile(codePath)
 
-        elif 'email to harry' in query:
+        elif 'email to foobar' in query:
             try:
-                speak("What should I say?")
+                speak("What should I write?")
                 content = takeCommand()
-                to = "harryyourEmail@gmail.com"    
+                to = "foobar@gmail.com"
                 sendEmail(to, content)
-                speak("Email has been sent!")
+                speak("Email sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
-        
+                speak("Sorry. I am not able to send this email")
+
         elif 'stop' in query:
             print(f'exiting smart assisstant')
             speak('Thank you for giving me a chance to help you. Have a Good Day!')
